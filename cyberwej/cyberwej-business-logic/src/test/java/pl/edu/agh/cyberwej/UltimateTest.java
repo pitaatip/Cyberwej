0fe1d5c5eaf313afa71f3191c3ef01726134bd4c
Tomasz Wojcik <tomekwo@tlen.pl>
2012-04-09 18:12:35 +0200

    Project directory structure added; gitignore added

---
+++ cyberwej/cyberwej-business-logic/src/test/java/pl/edu/agh/cyberwej/UltimateTest.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.cyberwej;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UltimateTest {
+
+	@Test
+	public void test() {
+		assertEquals(2 + 2, 4);
+	}
+
+}
