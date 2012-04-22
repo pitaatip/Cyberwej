ff49f86673c90a13305ab9bac23cd1c4f5da3cfa
Tomasz Wojcik <tomekwo@tlen.pl>
2012-04-22 13:03:57 +0200

    Adding another simple test to check jenkins response

---
+++ cyberwej/cyberwej-business-logic/src/test/java/pl/edu/agh/cyberwej/ThirdTest.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.cyberwej;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ThirdTest {
+
+	@Test
+	public void testEverything() {
+		String a = "a";
+		String b = "b";
+		assertEquals("ab", a + b);
+	}
+
+}
