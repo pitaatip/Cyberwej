5cf352cb9e2de4185bf7551501d4ef712614fb1c
Krzysztof Wilaszek <k.wilaszek@gmail.com>
2012-04-24 19:07:32 +0200

    Sequence diagram for new payment creation added

--- cyberwej/cyberwej-data/src/main/java/org/agh/iosr/cyberwej/data/dao/interfaces/UserDAO.java
+++ cyberwej/cyberwej-data/src/main/java/org/agh/iosr/cyberwej/data/dao/interfaces/UserDAO.java
@@ -8,7 +8,7 @@ public interface UserDAO {
 	public boolean saveUser(User user);
 
 	public User findUserByMail(String mail);
-	
+
 	public User findUserById(int id);
 
 	public void removeUser(User user);
