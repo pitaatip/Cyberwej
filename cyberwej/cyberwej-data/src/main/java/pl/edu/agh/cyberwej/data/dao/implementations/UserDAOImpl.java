a099cf832bcf3027b719968e5f7976bbfd906032
Tomasz Wojcik <tomekwo@tlen.pl>
2012-05-19 22:29:04 +0200

    #18 Add ability to find user by criteria

--- cyberwej/cyberwej-data/src/main/java/pl/edu/agh/cyberwej/data/dao/implementations/UserDAOImpl.java
+++ cyberwej/cyberwej-data/src/main/java/pl/edu/agh/cyberwej/data/dao/implementations/UserDAOImpl.java
@@ -2,6 +2,7 @@ package pl.edu.agh.cyberwej.data.dao.implementations;
 
 import java.util.List;
 
+import static org.springframework.util.StringUtils.hasText;
 import org.springframework.stereotype.Repository;
 
 import pl.edu.agh.cyberwej.data.dao.interfaces.UserDAO;
@@ -20,8 +21,9 @@ public class UserDAOImpl extends DAOBase<User> implements UserDAO {
     public User findUserByMail(String mail) {
         List<User> queryResult = this.hibernateTemplate.find(
                 "from User user where user.mail=?", mail);
-        if (queryResult.size() > 0)
+        if (queryResult.size() > 0) {
             return queryResult.get(0);
+        }
         return null;
     }
 
@@ -42,8 +44,9 @@ public class UserDAOImpl extends DAOBase<User> implements UserDAO {
     public User findUserById(int id) {
         List<User> queryResult = this.hibernateTemplate.find(
                 "from User user where user.id=?", id);
-        if (queryResult.size() > 0)
+        if (queryResult.size() > 0) {
             return queryResult.get(0);
+        }
         return null;
     }
     
@@ -52,9 +55,33 @@ public class UserDAOImpl extends DAOBase<User> implements UserDAO {
     public User findUserByLogin(String login) {
         List<User> queryResult = this.hibernateTemplate.find(
                 "from User user where user.login=?", login);
-        if (queryResult.size() > 0)
+        if (queryResult.size() > 0) {
             return queryResult.get(0);
+        }
         return null;
     }
     
+    private String toLikeParameter(String parameter) {
+        if(hasText(parameter)) {
+            StringBuilder sb = new StringBuilder("%");
+            sb.append(parameter);
+            sb.append("%");
+            return sb.toString();
+        }
+        return "%";
+    }
+    
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> findUserByCriteria(String login, String name, String surname, String location) {
+        String loginLike = toLikeParameter(login);
+        String nameLike = toLikeParameter(name);
+        String surnameLike = toLikeParameter(surname);
+        String locationLike = toLikeParameter(location);
+        
+        List<User> queryResult = this.hibernateTemplate.find(
+        "from User user where user.login like ? and user.name like ? and user.surname like ? and user.location like ?",
+        loginLike, nameLike, surnameLike, locationLike);
+        return queryResult;
+    }
 }
