ecf6255459d4e7a4bcb7c56f761c31a510a52d12
Tomasz Wojcik <tomekwo@tlen.pl>
2012-05-20 09:33:04 +0200

    #18 Add transactional method to save group with its members

--- cyberwej/cyberwej-business-logic/src/main/java/pl/edu/agh/cyberwej/business/services/impl/GroupServiceImpl.java
+++ cyberwej/cyberwej-business-logic/src/main/java/pl/edu/agh/cyberwej/business/services/impl/GroupServiceImpl.java
@@ -1,17 +1,20 @@
 package pl.edu.agh.cyberwej.business.services.impl;
 
 import java.util.HashSet;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import pl.edu.agh.cyberwej.business.services.api.GroupMembershipService;
 import pl.edu.agh.cyberwej.business.services.api.GroupService;
 import pl.edu.agh.cyberwej.data.dao.interfaces.GroupDAO;
 import pl.edu.agh.cyberwej.data.objects.Group;
 import pl.edu.agh.cyberwej.data.objects.GroupMembership;
 import pl.edu.agh.cyberwej.data.objects.Payment;
+import pl.edu.agh.cyberwej.data.objects.User;
 
 /**
  * 
@@ -23,12 +26,35 @@ public class GroupServiceImpl implements GroupService {
 
     @Autowired
     private GroupDAO groupDAO;
+    
+    @Autowired
+    private GroupMembershipService groupMembershipService;
 
     @Override
     public boolean saveGroup(Group group) {
         return this.groupDAO.saveGroup(group);
     }
 
+    @Override
+    @Transactional
+    public boolean saveGroupWithItsMembers(Group group, List<User> members) {
+        if(groupDAO.getGroupByName(group.getName()) != null) {
+            return false;
+        }
+        if(!groupDAO.saveGroup(group)) {
+            return false;
+        }
+        for(User user : members) {
+            groupMembershipService.addGroupMember(group, user);
+        }
+        return true;
+    }
+    
+    @Override
+    public void setGroupMembershipService(GroupMembershipService service) {
+        this.groupMembershipService = service;
+    }
+    
     /**
      * @param groupDAO
      *            the groupDAO to set
