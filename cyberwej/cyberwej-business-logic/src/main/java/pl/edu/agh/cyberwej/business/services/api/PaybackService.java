6597d2b4b2ada94060910ec777bf6169c7bd4fa6
Tomasz Wojcik <tomekwo@tlen.pl>
2012-05-23 08:23:03 +0200

    Ignoring failing test for now

--- cyberwej/cyberwej-business-logic/src/main/java/pl/edu/agh/cyberwej/business/services/api/PaybackService.java
+++ cyberwej/cyberwej-business-logic/src/main/java/pl/edu/agh/cyberwej/business/services/api/PaybackService.java
@@ -2,6 +2,7 @@ package pl.edu.agh.cyberwej.business.services.api;
 
 import java.util.List;
 
+import pl.edu.agh.cyberwej.data.dao.interfaces.PaybackDAO;
 import pl.edu.agh.cyberwej.data.objects.Group;
 import pl.edu.agh.cyberwej.data.objects.Payback;
 import pl.edu.agh.cyberwej.data.objects.User;
@@ -21,4 +22,6 @@ public interface PaybackService {
     public boolean acceptPaybackById(int paybackId, boolean isAccepted);
     
     public List<Payback> getPaybacksForUser(User user, boolean onlyUnaccepted);
+    
+    public void setPaybackDAO(PaybackDAO paybackDAO);
 }
