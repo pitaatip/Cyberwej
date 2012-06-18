b26086c3a80d4e1c0f5e485bad4fe23feb0fd739
Tomasz Wojcik <tomekwo@tlen.pl>
2012-06-18 21:34:30 +0200

    #36 Sorting lists on main page

--- cyberwej/cyberwej-web/src/main/java/pl/edu/agh/cyberwej/web/beans/views/user/LoggedUserBean.java
+++ cyberwej/cyberwej-web/src/main/java/pl/edu/agh/cyberwej/web/beans/views/user/LoggedUserBean.java
@@ -9,6 +9,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
+import org.richfaces.component.SortOrder;
+
 import pl.edu.agh.cyberwej.business.services.api.PaymentService;
 import pl.edu.agh.cyberwej.business.services.api.UserService;
 import pl.edu.agh.cyberwej.data.objects.GroupMembership;
@@ -26,6 +28,8 @@ import pl.edu.agh.cyberwej.web.beans.common.SessionContextBean;
 public class LoggedUserBean {
 
     private static final String SELECTEDGROUP = "selectedGroup";
+    
+    private static final SortOrder ascendingOrder = SortOrder.ascending;
 
     @ManagedProperty(value = "#{userService}")
     private UserService userService;
@@ -107,5 +111,9 @@ public class LoggedUserBean {
      */
     public Map<Payment, Float> getLatestPaymentsMap() {
         return this.latestPaymentsMap;
+    }
+    
+    public SortOrder getAscendingOrder() {
+        return ascendingOrder;
     }
 }
